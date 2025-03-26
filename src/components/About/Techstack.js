74e290c69b8742af685d2aa379a33e5ce252b125
mahmoudbymassen <mahmoudbymassen1@gmail.com>
2025-03-26 11:01:26 +0000

    Initial commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+import {
+  DiJavascript1,
+  DiReact,
+  DiMongodb,
+  DiPython,
+  DiPhp,
+  DiCss3,
+  DiHtml5,
+  DiLaravel,
+  DiGit,
+  DiDocker,
+  DiNodejs, 
+} from "react-icons/di";
+
+import { 
+  SiExpress, 
+  SiPostgresql, 
+  SiMysql,
+  SiGithub 
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPhp />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiLaravel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs /> 
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import ecom from "../../Assets/Projects/ecom.png"
+import managment from "../../Assets/Projects/managment.png";
+import resume from "../../Assets/Projects/resume.png";
+import chatify from "../../Assets/Projects/chatify.png";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="WeChat"
+              description="This beginner-level Chatbot is a simple yet functional application designed to simulate basic conversational interactions. Built with PHP for backend logic, CSS for a clean and user-friendly interface, and SQL for data storage, the chatbot serves as an introductory project to explore the fundamentals of web development and AI-driven interactions."
+              ghLink="https://github.com/mahmoudbymassen/we-chat"
+
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={managment}
+              isBlog={false}
+              title="projects management"
+              description="This Project Management System is a powerful tool designed to streamline task organization, team collaboration, and project tracking. Built with Laravel for robust backend functionality, SQL for efficient database management, and a combination of CSS and JavaScript for a dynamic and responsive user interface, the system provides a seamless experience for managing projects and their associated tasks."
+              ghLink="https://github.com/mahmoudbymassen/project-management"
+
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ecom}
+              isBlog={false}
+              title="E-commerce"
+              description="This E-Commerce Platform is a feature-rich, full-stack web application designed to deliver a seamless online shopping experience. Built with React.js for a fast and interactive frontend, Express.js for a scalable and efficient backend, and MongoDB Atlas for cloud-based database management, the platform combines modern web development technologies to create a robust and user-friendly solution."
+              ghLink="https://github.com/mahmoudbymassen/e-commerce"
+             
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={resume}
+              isBlog={false}
+              title="Resume Creator"
+              description="Developed a user-friendly Resume Creator web application using React.js to help users build professional resumes effortlessly. The application allows users to input their personal, educational, and professional details, which are dynamically rendered into a well-structured and visually appealing resume template."
+              ghLink="https://github.com/mahmoudbymassen/Resume"
+
+            />
+          </Col>
+
+
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
